fd4776ea0d86cc6f5b53d2282942219417f082e2
eddiespag-hetti <eddie_p89@hotmail.com>
2024-03-20 19:38:57 +1100

    Fixed bugs
    Got routes to work correctly
    
    added styling and Bootstrap to .handlebars files.

--- utils/helpers.js
+++ utils/helpers.js
@@ -10,7 +10,7 @@ module.exports = {
   get_emoji: () => {
     const randomNum = Math.random();
 
-    // Return a random emoji
+    // Return a random emoji to the Blog Posts
     if (randomNum > 0.7) {
       return `<span for="img" aria-label="lightbulb">💡</span>`;
     } else if (randomNum > 0.4) {
